18ca0fbe5b1fac046e0871f7aeb0d12459188856
ClaireDL <cl.delleluche@gmail.com>
2019-08-20 16:26:23 +0100

    worked on areaCalculator

--- src/areaCalculator.scala
+++ src/areaCalculator.scala
@@ -1,57 +1,24 @@
 
-object areaCalculator {
+object Main extends App {
+  val myListOfShapes: Seq[Shape] = Seq(
+    new Circle("piada", 4)
+  )
 
-  def main( args: Array[String] ): Unit = {
-    val piada = new Circle
-    var result = piada.area(4 )
-    println(s"piada: $result")
-
-    val toast = new Square
-    result = toast.area(5 )
-    println(s"toast: $result")
-
-    val biscuit = new Rectangle
-    result = biscuit.area( 3, 5)
-    println(s"biscuit: $result")
-
-    val harlequin = new Rhombus
-    result = harlequin.area( 3, 4)
-    println(s"harlequin: $result")
-
-    val croissant = new Triangle
-    result = croissant.area( 8, 12)
-    println(s"croissant: $result")
- }
-}
-
-abstract class Shape {}
-
-class Circle extends Shape {
-  def area ( diameter: Double ): Double = {
-    diameter * math.Pi - ( diameter * math.Pi % 0.01 )
+  for { shape <- myListOfShapes } {
+    println(s"The area of ${shape.name} is ${shape.area}")
   }
 }
 
-class Square extends Shape {
-  def area ( side: Double ): Double = {
-    math.pow( side, 2 )
+// Abstract class
+abstract class Shape(shapeName: String, dimension: Double) {
+  var name: String = shapeName
+  def area: Double = {
+    3 * 3
   }
 }
 
-class Rectangle extends Shape {
-  def area ( side1: Double, side2: Double ): Double = {
-    side1 * side2
-  }
-}
-
-class Rhombus extends Shape {
-  def area ( side1: Double, side2: Double ): Double = {
-    side1 * side2
-  }
-}
-
-class Triangle extends Shape {
-  def area ( base: Double, height: Double ): Double = {
-    ( base * height ) / 2
+class Circle(_shapeName: String, diameter: Double) extends Shape(_shapeName, diameter) {
+    override def area: Double = {
+    diameter * math.Pi
   }
 }
